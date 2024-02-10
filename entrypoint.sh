236d8a53fb821d2c8b3f595840e20ff95832cf6f
kopfan <kopfan@posteo.de>
2024-02-10 20:29:24 +0100

    original

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Hello! Your AppID is: $1"
 time=$(date)
 echo "::set-output name=time::$time"
-b4a version > b4a-version.txt
-cat b4a-version.txt
+#b4a version > b4a-version.txt
+#cat b4a-version.txt
